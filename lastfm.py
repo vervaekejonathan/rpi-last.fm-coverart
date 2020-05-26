199b14285918905a9cd20b909b3f96d37c47a18e
jonathanvervaeke <vervaeke.jonathan@gmail.com>
2020-05-26 20:01:03 +0200

    Initial commit

---
+++ lastfm.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import pylast
+import configparser
+
+def connect():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    if 'last.fm' in config:
+        API_KEY = config['last.fm']['API_KEY']
+        API_SECRET = config['last.fm']['API_SECRET']
+        username = config['last.fm']['username']
+        network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
+                                        username=username)
+        user = network.get_user(username)
+        return network, user
+
+def getuser():
+    try:
+        network, user = connect()
+        return user.get_now_playing() 
+    except Exception as e:
+        print(e)
+        return None
+
+
+def update_album_art():
+    #try:
+        network, user = connect()
+        track = user.get_now_playing()    
+        album = track.get_album()
+
+        image = None
+
+        if album is not None:
+            image = album.get_cover_image()
+
+        #if album is None or image is None:
+        #    print("no album found using alternative")
+        #    artist_name = track.get_artist()
+        #    album_name = track.get_album()
+        #    album = network.get_album(artist_name, album_name)
+        #    image = album.get_cover_image()
+
+        if album is None or image is None:
+            print("no album found using artist instead")
+            artist_name = track.get_artist()
+            artist = network.get_artist(artist_name)
+            image = artist.get_cover_image()
+
+        if image == "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png":
+            print("no cover available")
+            image = None
+        return image
+    #except Exception as e:
+    #    print(e)
+    #    return None
+    
+if __name__ == "__main__":
+    getuser()
---
+++ sound.py
@@ -0,0 +1,130 @@
+from guizero import App, Text, Picture
+import spotipy
+import lastfm
+import _thread 
+import configparser
+import time
+import sys
+import os
+from urllib import request
+
+app = None
+message = None
+pic = None
+toggle = 0
+current_track = ""
+current_cover_art = ""
+
+# the screensaver disrupts the application (on RPI), so we send a disactive command periodically
+def screensaver(onoff):
+    os.system('xscreensaver-command -' + ('de' if not onoff else '') + 'activate > /dev/null')
+
+def disable_screensaver():
+    screensaver(False)
+
+# hide content when no relevant information is received
+def hide_content():
+    message.hide()
+    pic.hide()
+
+# show content if possible
+# calling show 2 times after eachother causes the application to flicker, so we avoid that
+def show_content():
+    if not message.visible:
+        message.show()
+    if not pic.visible:
+        pic.show()
+
+# get the new information from lastfm and try to display it
+def update_message():
+    global message
+    global pic
+    global current_track
+
+    # get track from lastfm
+    disable_screensaver()
+    new_track = lastfm.getuser()    
+
+    if new_track is not None:
+
+        show_content()
+
+        # only update if new content
+        if str(new_track) != str(current_track):
+
+            current_track = new_track
+            print("-------------------------------")            
+            print(str(current_track))
+
+            # get the album that matches the track
+            album = current_track.get_album();
+            album_title = "" if album is None else album.title
+
+            # format the artist, title and album
+            message.value = str(current_track.artist) + "\n" + current_track.title + "\n" + str(album_title)
+
+            # handle cover art
+            cover_art = lastfm.update_album_art()
+            print(cover_art)
+            pic.after(0, update_album_art, args=[cover_art])
+
+    else:
+        hide_content()
+
+# update the cover art
+def update_album_art(cover_art):
+    global pic
+    global current_cover_art
+
+    if cover_art is not None:
+
+        # only update if new cover art
+        if current_cover_art is not cover_art:
+
+            current_cover_art = cover_art
+            image_name = '.coverart.png'
+
+            # download image and update content
+            request.urlretrieve(current_cover_art, image_name)
+            pic.image = image_name   
+
+    else:        
+        pic.image = 'black.png'   
+
+# load settings and setup the application
+def start():
+    global app
+    global message
+    global pic
+
+    # load config file
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    if 'settings' in config:
+
+        #load settings
+        settings = config['settings']
+        width = int(settings['width'])
+        height = int(settings['height'])
+        font = settings['font']
+        font_size = int(settings['font-size'])
+        font_color = settings['font-color']
+        refresh_time = int(settings['refresh-time']) * 1000
+
+        # create UI elements
+        app = App(title="Cover Art", bg="black")
+        pic = Picture(app, image='black.png', width=width, height=height)
+        message = Text(app, text="Cover Art", size=font_size, font=font, color=font_color)
+
+        # callback function that is called every #refresh_time seconds
+        message.repeat(refresh_time, update_message)
+
+        # display all!
+        app.set_full_screen()
+        app.display()
+
+if __name__ == "__main__":
+    start()
+
+
