b7dee5c9ab10baa8a8baf3cc8f7a86008f3bf4ad
jonathanvervaeke <vervaeke.jonathan@gmail.com>
2020-06-01 11:34:08 +0200

    Better album titles

--- lastfm.py
+++ lastfm.py
@@ -3,6 +3,7 @@
 import pylast
 import configparser
 
+
 def connect():
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -11,14 +12,15 @@ def connect():
         API_SECRET = config['last.fm']['API_SECRET']
         username = config['last.fm']['username']
         network = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
-                                        username=username)
+                                       username=username)
         user = network.get_user(username)
         return network, user
+
 
 def getuser():
     try:
         network, user = connect()
-        return user.get_now_playing() 
+        return user.get_now_playing()
     except Exception as e:
         print(e)
         return None
@@ -27,19 +29,11 @@ def getuser():
 def update_album_art():
     try:
         network, user = connect()
-        track = user.get_now_playing()    
-        album = track.get_album()
-
-        image = None
-
-        if album is not None:
-            image = album.get_cover_image()
-
-        if album is None or image is None:
-            print("no album found using artist instead")
-            artist_name = track.get_artist()
-            artist = network.get_artist(artist_name)
-            image = artist.get_cover_image()
+        track = user.get_now_playing()
+        print(track)
+        artist = track.artist
+        album = track.album
+        image = track.image
 
         if image == "https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png":
             print("no cover available")
@@ -48,6 +42,7 @@ def update_album_art():
     except Exception as e:
         print(e)
         return None
-    
+
+
 if __name__ == "__main__":
     getuser()
